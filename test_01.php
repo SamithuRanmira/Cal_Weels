be89444a7f73a4175337ddf3bd0e12d17d9838cb
Samithu Ranmira <88590391+SamithuRanmira@users.noreply.github.com>
2023-09-19 12:18:48 +0530

    initial commit

---
+++ test_01.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$num1="";
+$num2="";
+$sym="";
+$ans="";
+if(isset($_POST["s1"]))
+{
+$num1=$_POST['num1'];
+$num2=$_POST['num2'];
+$ans=$num1+$num2;
+}
+if(isset($_POST["s2"]))
+{
+    $num1=$_POST['num1'];
+    $num2=$_POST['num2'];
+$ans=$num1-$num2;
+}
+
+if(isset($_POST["s3"]))
+{
+    $num1=$_POST['num1'];
+    $num2=$_POST['num2'];            
+$ans=$num1*$num2;
+}
+
+if(isset($_POST["s4"]))
+{
+$num1=$_POST['num1'];
+$num2=$_POST['num2'];         
+$ans=$num1/$num2;
+}
+
+if(isset($_POST["s5"]))
+{
+$num1=$_POST['num1'];
+$num2=$_POST['num2'];
+    $num1="";
+    $num2="";
+    $ans="";
+}
+?>
+
+    <form method="POST" class="form1">
+        <div class="container_01">
+            <div class="container_02">
+                <div class="inputs">
+                    <div>
+                        Number 1: <input type="number" name="num1" value="<?php echo ($num1)?>">
+                    </div>
+                    <div>
+                        Number 2: <input type="number" name="num2" value="<?php echo ($num2)?>"><br>
+                    </div>
+                    <div>
+                        Answer&nbsp;&nbsp;&nbsp;: <input type="number" name="ans" value="<?php echo $ans; ?>">
+                    </div>
+                </div>
+                <div class="container_03">
+                    <div class="operators">
+                        <div>
+                            <input type="submit" value="+" name="s1">
+                        </div>
+                        <div>
+                            <input type="submit" value="-" name="s2">
+                        </div>
+                        <div>
+                            <input type="submit" value="*" name="s3">
+                        </div>
+                        <div>
+                            <input type="submit" value="/" name="s4">
+                        </div>
+                        <div>
+                            <input type="reset" value="clear" name="s">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
